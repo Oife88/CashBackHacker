bfb3d2e2e3c3a3d10b468635ef2dfa04c1783606
Evgenia <z9262792@yandex.ru>
2023-02-03 14:26:15 +0300

    Testng commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+
+    @Test
+    public void testRemain2() {
+        int amount = 900;
+        CashbackHackService service = new CashbackHackService();
+        int actual = 100;
+        int expected = service.remain(amount);
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void testRemain3() {
+        int amount = 2000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = 0;
+        int expected = service.remain(amount);
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void testRemain() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = 0;
+        int expected = service.remain(amount);
+        assertEquals(actual,expected);
+
+    }
 }
